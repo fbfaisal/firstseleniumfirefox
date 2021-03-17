c067d6a59849c707db00a252013584e58aad1ec8
fbfaisal <fbfaisal.qa@gmail.com>
2021-03-17 18:46:39 -0400

    first commit

---
+++ firstseleniumfirefox/src/main/java/firstseleniumfirefox/FacebookTest.java
@@ -0,0 +1,19 @@
+package firstseleniumfirefox;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class FacebookTest {
+
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.gecko.driver", "C:\\Users\\fbfai\\Desktop\\D\\Drivers\\firefox\\geckodriver-v0.19.1-win64\\geckodriver.exe");
+		
+		WebDriver driver=new FirefoxDriver();
+		
+		driver.get("https://www.facebook.com");
+		
+		
+	}
+
+}
